0b7e7ea5b6111f321932a8070f5c98f57a10bc01
aduterte <alexduterte@gmail.com>
2020-11-09 22:09:58 -0500

    functional

--- src/App.js
+++ src/App.js
@@ -10,22 +10,43 @@ function App() {
     evidence = useRecoilValue(evidenceAtom),
     [foundEvidence, setFoundEvidence] = useState([]),
     [possibleGhosts, setPossibleGhosts] = useState(ghosts),
-    [possibeEv, setPossibleEv] = useState(evidence)
+    [possibleEv, setPossibleEv] = useState(evidence)
 
   function handleEvidence(e){
     let newEv = e.target.innerText
     setFoundEvidence([...foundEvidence, newEv])
+    let posGhosts = possibleGhosts.filter( x => x.evidence.find(e => e === newEv))
+    setPossibleGhosts(posGhosts)
+ 
+    let remaining = Array.from(new Set(posGhosts.map(x => x.evidence).flat()))
+    // finds all possible remaining evidence
+    let temp = remaining.filter(x=> {
+      if (!foundEvidence.includes(x) && x !==newEv){
+        return x
+      }
+    })
+    // removes evidence from evidence pool
+    console.log(temp)
+    setPossibleEv(temp)
+    // 
+    // console.log(temp)
+    // temp = possibleEv.filter(x=> x !== newEv)
+    // console.log(temp)
+    // setPossibleEv(temp)
   }
   
   return (
     <div className="App">
       <div>
+        <h3>Found Evidence</h3>
         {foundEvidence.map(f => <div key={f}>{f}</div>)}
       </div>
       <div>
-        {evidence.map(ev => <div onClick={handleEvidence} key={ev}>{ev}</div>)}
+        <h3>Possible Evidence</h3>
+        {possibleEv.map(ev => <div onClick={handleEvidence} key={ev}>{ev}</div>)}
       </div>
       <div>
+        <h3>Possible Ghosts</h3>
         {possibleGhosts.map(ghost=> <GhostComponent key={ghost.name} ghost={ghost}/>)}
       </div>
     </div>
