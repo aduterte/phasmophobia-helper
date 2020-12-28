75a15da4e431037d535ce5bf6b804a11332d5202
aduterte <alexduterte@gmail.com>
2020-12-27 21:59:27 -0500

    splitting up site into different sections

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default function(){
     return(
         <div>
-            
+            <div>
+                Maps
+            </div>
+            <div>
+                Evidence Tracker
+            </div>
         </div>
     )
 }
